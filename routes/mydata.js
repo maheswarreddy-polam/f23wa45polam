1a1bc9b5e87708873c5865f33b68010c8c3923e5
Polam <S562045@nwmissouri.edu>
2023-10-24 10:53:36 -0500

    app.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  res.render('mydata', { title: 'Maheswar Reddy Polam' });
+res.render('mydata', { title: 'Maheswar Reddy Polam' });
 });
 
 module.exports = router;
