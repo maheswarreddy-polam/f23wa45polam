7c3661fa15607634a29cdea84f66dbf6d54bd63d
Polam <S562045@nwmissouri.edu>
2023-10-24 11:24:14 -0500

    app.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,26 +1,14 @@
- // routes/computation.js
+var express = require('express');
+var router = express.Router();
 
-const express = require('express');
-const router = express.Router();
-
-// Define the route handler for the /computation endpoint
-router.get('/', (req, res) => {
-  // Check if there's a query parameter named 'x'
-  const queryX = req.query.x;
-  
-  // Generate a random value or use the provided 'x' value
-  const x = queryX ? parseFloat(queryX) : Math.random() * 10; // Adjust the range as needed
-
-  // Apply the Math.cos() function to 'x'
-  const result = Math.cos(x);
-
-  // Create the response message
-  const response = `${Math.cos.name} applied to ${x} is ${result}`;
-
-  // Send the response to the client
-  res.send(response);
-  const computationRouter = require('./routes/computation');
-app.use('/computation', computationRouter); 
+router.get('/', function(req, res,next) {
+ if(req.query.x==undefined) {
+    rm = Math.random();
+ } 
+ else {
+    rm = req.query.x;
+ }
+ var result = Math.cos(rm);
+res.render('computation', { func: 'Math.cos(${rm} is ${result}'});
 });
-
 module.exports = router;
