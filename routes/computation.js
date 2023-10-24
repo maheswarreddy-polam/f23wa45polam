c48f8c093dc6f7b630160b67fdfccafae716dd4a
Polam <S562045@nwmissouri.edu>
2023-10-24 12:27:42 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res,next) {
- if(req.query.x==undefined) {
-    rm = Math.random();
- } 
- else {
-    rm = req.query.x;
- }
- var result = Math.cos(rm);
-res.render('computation', { func: `Math.cos(${rm}) is ${result}`});
+router.get('/', function(req, res, next) {
+  try {
+    let rm;
+    if (req.query.x === undefined) {
+      rm = Math.random();
+    } else {
+      rm = parseFloat(req.query.x);
+    }
+
+    if (isNaN(rm)) {
+      // Handle the case where 'x' is not a valid number
+      res.status(400).send('Invalid value for "x"');
+      return;
+    }
+
+    var result = Math.cos(rm);
+    res.render('computation', { func: `Math.cos(${rm}) is ${result}` });
+  } catch (error) {
+    // Handle any unexpected errors
+    next(error);
+  }
 });
+
 module.exports = router;
+
